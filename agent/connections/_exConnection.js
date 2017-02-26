dd81cfd6c56c4a4debb7ee3988a665b60f63fea5
XeliteXirish <shaunoneill7@me.com>
2017-02-26 20:36:52 +0000

    Docs

--- agent/connections/_exConnection.js
+++ agent/connections/_exConnection.js
@@ -1,8 +1,14 @@
+/**
+ * Basic info about the connection
+ */
 exports.info = {
     name: 'Example Connection',
     enabled: false
 }
 
+/**
+ * Function that is ran on startup!
+ */
 exports.runPlugin = function () {
 
 };
