20a2d80041e7950cec1d654c26b55b091477e65c
Newtsrock <andrewtfluck@gmail.com>
2017-02-27 15:33:25 -0500

    React and CSS stuff

--- web/src/js/Components/Routes/404.js
+++ web/src/js/Components/Routes/404.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router'
 const Login = (props) => (
     <div>
         <div className="heading">
-            <span className="small-nonfix">Page Not Found</span>
+            <span className="small-nonfix">Page Not Found.</span>
         </div>
         <Link to="/" className="link">Go Home</Link>
     </div>
