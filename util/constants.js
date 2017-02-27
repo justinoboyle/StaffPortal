fa476c74311dc25d38b8a914976a46c29c95fa47
iaminventor02 <iaminventor02@gmail.com>
2017-02-27 11:59:00 -0500

    add constants

---
+++ util/constants.js
@@ -0,0 +1,15 @@
+/**
+ * Constant values
+ */
+
+/**
+ * Events that SP sends
+ */
+module.exports.EVENTS = { // Really just a sample of what this file is used for.
+  PUNISHMENT_ADDED: "punishment_added",
+  PUNISHMENT_EDIETD: "punishment_edited",
+  PUNISHMENT_DELETED: "punishment_deleted",
+  USER_AUTH_SUCCESS: "user_auth_success",
+  USER_AUTH_FAILURE: "user_auth_failed",
+  USER_AUTH_LOGOUT: "user_auth_logged_out"
+}
